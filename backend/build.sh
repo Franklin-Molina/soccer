091bd14ef12d98fb4375b5568e2e3a5f74fd7cbf
franklin-molina <asdfglxl7@gmail.com>
2026-02-13 17:58:05 -0500

    superuser

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,26 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+#!/bin/sh
+
+
+echo "Creando superusuario si no existe..."
+python manage.py shell << END
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("Superusuario creado")
+    else:
+        print("El superusuario ya existe")
+END
+
+echo "Iniciando servidor..."
+gunicorn cancha.wsgi:application --bind 0.0.0.0:8000
