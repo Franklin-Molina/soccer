0e686214fac252f84e119ee929e5c9ec658f60ee
franklin-molina <asdfglxl7@gmail.com>
2026-03-13 18:28:11 -0500

    diseño descripcion

--- frontend/src/presentation/components/Courts/CourtInfoSection.jsx
+++ frontend/src/presentation/components/Courts/CourtInfoSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CourtInfoSection({ court }) {
   return (
     (court.description || court.characteristics) && (
-      <div className="bg-white dark:bg-slate-800/50 backdrop-blur-sm border border-slate-200 dark:border-slate-700 rounded-xl p-4 mb-6">
+      <div className=" mt-4 mb-4 bg-white dark:bg-slate-800/50 backdrop-blur-sm border border-slate-200 dark:border-slate-700 rounded-xl p-4 ">
         {court.description && (
           <div className="mb-4">
             <h3 className="text-xl font-bold text-emerald-500 dark:text-emerald-400 mb-2">Descripción</h3>
